3cf003c573bb0b79fc78da5a90ee2ab98880b6f6
tovgar1 <fasthungara@gmail.com>
2024-02-17 14:32:51 +0600

    Done

--- task01_sem1/Program.cs
+++ task01_sem1/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.WriteLine("Введите первое целое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе целое число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+bool result = num1 == num2 * num2;
+string resultStr = result ? "да" : "нет";
+Console.WriteLine($"a = {num1}, b = {num2}, {resultStr}");
+
+

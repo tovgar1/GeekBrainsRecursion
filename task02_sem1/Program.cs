57801b18f142820e93ba04c6b22cf1ecf4bd689c
tovgar1 <fasthungara@gmail.com>
2024-02-17 14:55:15 +0600

    Done 2

--- task02_sem1/Program.cs
+++ task02_sem1/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+Console.WriteLine("Введите целое натуральное число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num <= 0)
+{
+
+    Console.WriteLine("Некорректный ввод!");
+    return;
+}
+
+
+int count = -num;
+
+while (count <= num)
+{
+
+    Console.WriteLine(count + "");
+    count = count + 1;
+
+}
